9385659e77c7b06480d9cd3b5608c3f63cd46ad9
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-24 14:05:45 -0300

    responsive: footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="w-full max-w-screen-2xl mx-auto mt-14">
+    <footer className="w-full mx-auto mt-14">
       <div className="text-base font-semibold text-gray-500 flex flex-col gap-6 items-center py-6 px-16 lg:px-fluid-16-64 bg-gray-300/40">
-        &copy; VENN STUDIO &middot; All rights reserved
+        <div className="flex flex-wrap gap-2 justify-center">
+          <span>&copy; VENN STUDIO &middot; </span>
+          <span>All rights reserved</span>
+        </div>
       </div>
     </footer>
   );
