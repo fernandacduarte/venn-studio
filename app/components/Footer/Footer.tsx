5494118919ce26664411baec816cfb46893112d5
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-25 15:23:02 -0300

    style: Footer: spacing

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full mx-auto mt-14">
       <div className="text-base font-semibold text-gray-500 flex flex-col gap-6 items-center py-6 px-16 lg:px-fluid-16-64 bg-gray-300/40">
-        <div className="flex flex-wrap gap-2 justify-center">
+        <div className="flex flex-wrap gap-1 justify-center">
           <span>&copy; VENN STUDIO &middot; </span>
           <span>All rights reserved</span>
         </div>
