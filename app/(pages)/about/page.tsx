8418298ad42350fa84d3727734b45dfa6732c4fb
Pedro Franceschin <pbfranceschin@gmail.com>
2024-11-01 14:46:57 -0300

    about page content

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -44,7 +44,7 @@ const ServiceItem = ({ title, text } : { title: string, text: string}) => {
       className={`${show ? 'opacity-1 translate-x-0' : 'opacity-0 translate-x-6'} transition-all duration-500 delay-300 ease-out flex flex-col gap-4 max-w-[65ch] pt-4 px-2 min-[430px]:pl-6 min-[430px]:pt-0  border-t-2 min-[430px]:border-l-2 min-[430px]:border-t-0 border-black`}
     >
       <span className="text-2xl leading-normal min-[375px]:text-[1.75rem] min-[375px]:leading-normal min-[500px]:text-3xl min-[500px]:leading-normal font-semibold">{title}</span>
-      <p className="text-xl min-[500px]:text-2xl leading-[2.3rem] min-[500px]:leading-[2.3rem] font-light">{text}</p>
+      <p className="text-xl min-[500px]:text-2xl leading-[2rem] min-[500px]:leading-[2.3rem] font-light">{text}</p>
     </div>
   );
 };
@@ -90,9 +90,9 @@ const AboutPage = () => {
             </div>
           </div>          
         </div>
-        <div className="flex flex-col gap-12 items-center mt-6 ">
-          <span className="text-4xl text-center max-w-[26ch] text-2xl leading-normal min-[375px]:text-3xl min-[375px]:leading-normal min-[500px]:text-4xl min-[500px]:leading-normal font-semibold">Have a game-changing idea? Skip the big agencies</span>
-          <span className="max-w-[53ch] text-center text-xl min-[375px]:text-2xl leading-normal min-[500px]:text-3xl min-[375px]:leading-normal min-[500px]:leading-normal font-light">
+        <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 lg:gap-12 items-center mt-6 ">
+          <span className="md:text-center max-w-[26ch] text-2xl leading-normal min-[375px]:text-3xl min-[375px]:leading-normal min-[500px]:text-4xl min-[500px]:leading-normal font-semibold">Have a game-changing idea? Skip the big agencies</span>
+          <span className="max-w-[53ch] md:text-center text-xl  leading-normal min-[500px]:text-2xl min-[375px]:leading-normal min-[500px]:leading-normal font-light">
             Our lean team works directly with founders to validate concepts and ship MVPs fast. Through rapid iteration and ruthless prioritization, we deliver products that users love and investors notice—without the overhead and complexity of large agencies. We're built for startups who need beautiful, functional products—without burning runway.
           </span>
         </div>
@@ -117,7 +117,7 @@ const AboutPage = () => {
             </div>
             <div className="flex flex-col min-[430px]:flex-row gap-6 items-center">
               <div className="opacity-50 w-[96px] ml-[-14px] mr-[14px]"><BlockchainIcon size={124} color={'hsl(77, 76%, 54%)'} stroke={3}/></div>       
-              <ServiceItem title={"Blockchain Integration"} text={"Curious if decentralization is the right path for your application? Talk to us, and we'll handle the rest. From identifying the best solutions for your needs to implementing a comprehensive suite of decentralized tools using industry standards."} />
+              <ServiceItem title={"Blockchain Integration"} text={"Evaluating blockchain for your product? We help businesses make the right call, then build it right—using industry-leading Web3 tools and protocols."} />
             </div>    
             {/* <ServiceItem title={"Full-Stack Development"} text={"We craft end-to-end web solutions, handling everything from frontend aesthetics to backend logic. Whether it's a fast, responsive website or a complex web application, we've got you covered."} />
             <ServiceItem title={"UI/UX Design"} text={"Our design team creates user-friendly interfaces that are not only visually appealing but also intuitive and engaging, making every interaction count."} />
