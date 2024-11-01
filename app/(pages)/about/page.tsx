2c934c48e231f148588b3e4831c20d1f55e9370f
Pedro Franceschin <pbfranceschin@gmail.com>
2024-11-01 14:19:32 -0300

    about: content

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -41,7 +41,7 @@ const ServiceItem = ({ title, text } : { title: string, text: string}) => {
   return (
     <div 
       ref={ref} 
-      className={`${show ? 'opacity-1 translate-x-0' : 'opacity-0 translate-x-6'} transition-all duration-500 ease-out flex flex-col gap-4 max-w-[65ch] pt-4 px-2 min-[430px]:pl-6 min-[430px]:pt-0  border-t-2 min-[430px]:border-l-2 min-[430px]:border-t-0 border-black`}
+      className={`${show ? 'opacity-1 translate-x-0' : 'opacity-0 translate-x-6'} transition-all duration-500 delay-300 ease-out flex flex-col gap-4 max-w-[65ch] pt-4 px-2 min-[430px]:pl-6 min-[430px]:pt-0  border-t-2 min-[430px]:border-l-2 min-[430px]:border-t-0 border-black`}
     >
       <span className="text-2xl leading-normal min-[375px]:text-[1.75rem] min-[375px]:leading-normal min-[500px]:text-3xl min-[500px]:leading-normal font-semibold">{title}</span>
       <p className="text-xl min-[500px]:text-2xl leading-[2.3rem] min-[500px]:leading-[2.3rem] font-light">{text}</p>
@@ -62,10 +62,10 @@ const AboutPage = () => {
         <div className="flex flex-col gap-6 max-w-[70ch] mt-10">
           {/* <Link href="/"><div className="w-full flex gap-1 opacity-70"><IconArrowLeft strokeWidth={1.5}/><span className="font-semibold">Back</span></div></Link> */}
           <p className="text-2xl leading-normal min-[375px]:text-3xl min-[375px]:leading-normal min-[500px]:text-4xl min-[500px]:leading-normal font-semibold text-balance">
-            We are a web development studio specializing in delivering tailor-made, <span className={`${styles.intro__highlight}`}>full-stack solutions</span>. 
+            We turn startup visions into <span className={`${styles.intro__highlight}`}>market-ready MVPs</span> in weeks, not months. 
           </p>
           <p className="text-xl leading-normal min-[375px]:text-2xl min-[375px]:leading-normal min-[500px]:text-3xl min-[500px]:leading-normal font-light">
-            From sleek UI design to seamless backend functionality, we build digital experiences that resonate with users and drive business growth. 
+            Our agile and highly skilled team specializes in helping founders validate their ideas and secure early traction through rapid, production-grade prototypes. From concept to launch, we deliver scalable full-stack solutions that grow with your business. 
           </p>
         </div>
         <div className="w-full">
@@ -82,13 +82,19 @@ const AboutPage = () => {
                 />
               </div>
             </div>
-            <div className="max-w-[60ch]">
+            <div className="max-w-[64ch]">
             {/* <div className="w-full"> */}
               <p className="text-xl min-[375px]:text-2xl leading-normal min-[500px]:text-3xl min-[375px]:leading-normal min-[500px]:leading-normal font-light">
-                Our mission is to bridge <span className="text-highlight font-semibold">creativity and technology</span>, empowering businesses with innovative web solutions that are both visually compelling and technically robust.
+                Our mission is to bridge <span className="text-highlight font-semibold">creativity and technology</span>, empowering starting businesses with innovative solutions that are both visually compelling and technically robust.
               </p>
             </div>
-          </div>
+          </div>          
+        </div>
+        <div className="flex flex-col gap-12 items-center mt-6 ">
+          <span className="text-4xl text-center max-w-[26ch] text-2xl leading-normal min-[375px]:text-3xl min-[375px]:leading-normal min-[500px]:text-4xl min-[500px]:leading-normal font-semibold">Have a game-changing idea? Skip the big agencies</span>
+          <span className="max-w-[53ch] text-center text-xl min-[375px]:text-2xl leading-normal min-[500px]:text-3xl min-[375px]:leading-normal min-[500px]:leading-normal font-light">
+            Our lean team works directly with founders to validate concepts and ship MVPs fast. Through rapid iteration and ruthless prioritization, we deliver products that users love and investors notice—without the overhead and complexity of large agencies. We're built for startups who need beautiful, functional products—without burning runway.
+          </span>
         </div>
         <div className="flex flex-col gap-[6rem] items-center justify-center my-4 mt-6">
           <span className="text-4xl min-[375px]:text-[2.5rem] min-[500px]:text-5xl font-semibold">What we offer</span>
