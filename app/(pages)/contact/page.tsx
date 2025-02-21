f563e12f1b1017ae12d590e2f480e5ab19c1e927
Pedro Franceschin <pbfranceschin@gmail.com>
2025-02-21 17:46:11 -0300

    /contact: layout, content

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -5,25 +5,35 @@ import styles from './page.module.css'
 
 const ContactPage = () => {
   return (
-    <div className="animate-fadeIn w-full max-w-screen-2xl mx-auto p-4 min-[375px]:p-6 md:px-16 lg:px-fluid-16-64 flex gap-6 flex-grow items-stretch">
-      <div className="h-full w-full flex flex-col gap-14 flex-grow text-neutralDark mt-12">
+    <div className="animate-fadeIn w-full max-w-screen-2xl mx-auto p-4 min-[375px]:p-6 md:px-16 lg:px-fluid-16-64 flex gap-6 flex-grow items-stretch text-xl md:text-2xl lg:text-[30px]">
+      <div className="w-full flex flex-col gap-14 flex-grow mt-12">
         {/* <Link href="/"><div className="w-full flex gap-2 opacity-70"><IconArrowLeft strokeWidth={1.2}/><span className="">Home</span></div></Link> */}
         {/* <div>
           <span className="text-6xl font-[family-name:var(--font-panamera-bold)] ">Projects</span>
         </div> */}
-        <div>
-          <span className="text-2xl min-[430px]:text-3xl md:text-4xl font-extrabold">Contact us!</span>
+        <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 lg:gap-12 max-w-[30ch]">
+          <h1>Contact us!</h1>
+          {/* <p className='leading-normal'>Ready to discuss your next project? Let's connect for a no-obligation consultation.</p> */}
         </div>
-        <div className='w-full flex flex-col gap-10 justify-center items-center mt-6'>
-          <div className={`flex gap-5 justify-center items-start px-10 py-6 bg-neutralDark text-neutralLight-100 rounded-2xl ${styles.button}`}>
+        <div className='w-full flex flex-col flex-1 gap-10 justify-center items-center mt-6'>
+          <p className='leading-normal max-w-[30ch] text-center'>Ready to discuss your next project? Let's connect for a no-obligation consultation.</p>
+          <div className={`flex gap-5 justify-center items-start px-10 py-6 bg-foreground text-background rounded-2xl ${styles.button}`}>
             <div className='text-[28px] sm:text-[32px] md:text-[38px]'><IconCalendar size={'1em'} stroke={2} /></div>
-            <span className='text-2xl sm:text-3xl md:text-4xl font-semibold '>Book a call</span>
+            <h2>Book a call</h2>
             {/* <span className='text-2xl font-semibold min-[500px]:hidden'>Send a message</span> */}
           </div>
-          <span className='text-2xl min-[430px]:text-3xl md:text-4xl font-bold'>or</span>
-          <div className="flex flex-wrap justify-center items-end text-xl min-[430px]:text-2xl md:text-3xl font-semibold">
-            e-mail us at&nbsp;<span className={`text-2xl min-[430px]:text-3xl md:text-4xl font-bold ${styles.email__highlight}`}>contact@vennstudio.dev</span>
-          </div>
+          {/* <h2 >or</h2>
+          <p className='text-2xl md:text-3xl lg:text-4xl'>
+            e-mail us at&nbsp;<span className={`font-bold ${styles.email__highlight}`}>contact@vennstudio.dev</span>
+          </p> */}
+        </div>
+        <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 max-w-[30ch]">
+          <span className={`font-bold w-fit ${styles.email__highlight}`}>contact@vennstudio.dev</span>
+          <span className='text-lg md:text-xl lg:text-2xl'>
+            CNPJ: 56.056.578/0001-14
+            <br/>
+            Tel: +55 21 96461 4862
+          </span>
         </div>
       </div>
     </div>
