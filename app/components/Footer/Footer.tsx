f352901850d21d7453e3fcd4c4a23e1a2e0e7363
Pedro Franceschin <pbfranceschin@gmail.com>
2025-02-21 09:41:06 -0300

    footer responsiveness

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="w-full mx-auto mt-14">
-      <div className="text-base font-semibold text-gray-500 flex flex-col gap-6 items-center py-6 px-16 lg:px-fluid-16-64 bg-gray-300/40">
-        <div className="flex flex-wrap gap-1 justify-center">
+      <div className="text-base font-semibold text-gray-500 flex flex-col gap-6 items-center py-10 md:py-12 lg:py-14 px-16 lg:px-fluid-16-64 bg-gray-300/40">
+        <div className="flex flex-wrap gap-1 justify-center text-xs md:text-sm lg:text-base">
           <span>&copy; VENN STUDIO &middot; </span>
           <span>All rights reserved</span>
         </div>
