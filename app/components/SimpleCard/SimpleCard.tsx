53f4933304d63db8e8ab932f12bfb5ce92b1e60d
fernandacduarte <fcduarte26@gmail.com>
2025-02-27 16:06:48 -0300

    style: improve responsiveness of home page cards

--- app/components/SimpleCard/SimpleCard.tsx
+++ app/components/SimpleCard/SimpleCard.tsx
@@ -3,7 +3,7 @@ import styles from './SimpleCard.module.css';
 const SimpleCard = ({ children, backgroundColor = '' }: { children: React.ReactNode, backgroundColor?: string }) => {
   return (
     <div 
-      className={`z-10 overflow-hidden h-full w-full rounded-3xl p-8 sm:p-10 flex-grow bg-transparent relative hover:scale-[1.03] active:scale-95 transition-transform duration-300 cursor-pointer ${styles['blur-background']}`}
+      className={`z-10 overflow-hidden h-full w-full rounded-3xl p-8 sm:p-8 lg:p-10 flex-grow bg-transparent relative hover:scale-[1.03] active:scale-95 transition-transform duration-300 cursor-pointer ${styles['blur-background']} max-[639px]:h-auto max-[639px]:flex max-[639px]:flex-grow max-[639px]:items-stretch`}
       // style={{ ...(backgroundColor ? { background: backgroundColor } : {}), boxShadow: 'rgba(149, 157, 165, 0.3) 0px 8px 24px' }}
       style={{ ...(backgroundColor ? { background: backgroundColor } : {}), boxShadow: 'rgba(149, 157, 165, 0.3) 0px 4px 24px' }}
     > 
--- app/components/Team/Team.tsx
+++ app/components/Team/Team.tsx
@@ -10,13 +10,13 @@ const Team = () => {
     // <BlurCard backgroundColor="rgba(255, 255, 255, 0.1)">
     // <SimpleCard backgroundColor="rgba(0, 0, 0, 0.15)">
     <SimpleCard backgroundColor="linear-gradient(340deg, rgba(0, 0, 0, 0.15) 70%, rgba(0, 0, 0, 0.1) 100%)">
-      <Link href="/team">
+      <Link href="/team" className="max-[639px]:w-full">
         <div className="h-full flex flex-col justify-between items-center font-medium">
           <div className="w-full flex justify-between items-center mt-[-12px]">
             <span className="text-[0.65rem] sm:text-xs min-[860px]:text-sm xl:text-base tracking-[0.05em] max-w-[14ch]">
               MEET OUR TEAM MEMBERS
             </span>
-            <div className="mr-[-10px] rotate-45 text-[2.5rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[4rem]">
+            <div className="mr-[-10px] rotate-45 text-[2.5rem] sm:text-[3rem] md:text-[3rem] lg:text-[4rem]">
               <IconArrowUpRight size={'1em'} strokeWidth={1.2} />
             </div>
           </div>
