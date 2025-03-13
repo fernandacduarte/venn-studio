a6ff2cccfcd82ca86040ed6be40950b9c4352046
Pedro Franceschin <pbfranceschin@gmail.com>
2025-03-13 14:53:40 -0300

    feat: integrate Carousel3D and CarouselSwipeable for enhanced project image display

--- app/components/ProjectDescription/ProjectDescription.tsx
+++ app/components/ProjectDescription/ProjectDescription.tsx
@@ -2,14 +2,34 @@ import { TwoColumnMarkdown } from "../TwoColumnMarkdown/TwoColumnMarkdown";
 import Link from "next/link";
 import { IconArrowRight } from "@tabler/icons-react";
 import styles from "./ProjectDescription.module.css";
-import ReactMarkdown from 'react-markdown';
 import Markdown from "../Markdown/Markdown";
 import { quantity, ProjectMetadata, resolveProjectsPathname } from "@/app/lib/projects";
 import SlowMotion from "../SlowMotion/SlowMotion";
+import Image from "next/image";
+import Carousel3D, { Carousel3DSlide } from "../Carousel3D/Carousel3D";
+import CarouselSwipeable from "../CarouselSwipeable/CarouselSwipeable";
+
 
 interface ProjectDescriptionProps extends ProjectMetadata {
     index: number
 }
+
+const carouselSwipeableSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    responsive: [
+        {
+          breakpoint: 436,
+          settings: {
+            centerMode: false
+          }
+        }
+    ]
+  };
+  
 
 export default function ProjectDescription ({
     index,
@@ -21,37 +41,61 @@ export default function ProjectDescription ({
 }: ProjectDescriptionProps) {
 
     return (
-        <div className="h-full w-full max-w-screen-2xl mx-auto md:px-16 min-[375px]:px-6 px-4 lg:px-fluid-16-64 flex flex-col gap-16 flex-grow mt-12">
-                <h1>{title}</h1>
-                <img
+        <div className="h-full w-full max-w-screen-2xl mx-auto flex flex-col gap-16 flex-grow mt-12 animate-fade-in">
+                <h1 className="px-4 md:px-16 min-[375px]:px-6 lg:px-fluid-16-64 ">{title}</h1>
+                {/* <img
                 src="/images/Frame321.png"
                 alt="img"
                 className="w-full max-w-[830px] mx-auto"
+                /> */}
+                <Carousel3D 
+                className="w-full px-16 lg:px-0 max-w-[800px] h-[500px] mx-auto hidden md:block"
+                slides={get3dCarouselItems(images)} 
+                offset={1.5}
                 />
-                <div className="relative w-full rounded-xl p-10 bg-gray-300 overflow-hidden"
-                style={{ boxShadow: 'rgba(149, 157, 165, 0.3) 0px 4px 24px' }}
-                >
-                    <div className="absolute w-[500px] md:w-[800px] right-0 top-[50%] -translate-y-1/2 translate-x-1/2 lg:translate-x-1/3 opacity-30">
-                        <SlowMotion className="w-full">
-                            <img
-                            src="/images/uuundulate9-dark.svg"
-                            alt="background"
-                            className="w-full"
-                            />
-                        </SlowMotion>
-                    </div>
-                    <h2 className="text-balance leading-normal">{subtitle}</h2>
+                <div className="w-full overflow-hidden md:hidden">
+                    <CarouselSwipeable settings={carouselSwipeableSettings} showNavigation>
+                        {images.map((item, index) => (
+                            <div key={index} className="px-4">
+                                <Image
+                                className="mx-auto"
+                                src={item}
+                                alt={`image-${index}`}
+                                width={400}
+                                height={400}
+                                />
+                            </div>
+                        ))}
+                    </CarouselSwipeable>
                 </div>
-                <TwoColumnMarkdown text={description} />
+                <div className="px-4 md:px-16 min-[375px]:px-6 lg:px-fluid-16-64">
+                    <div className="relative w-full rounded-xl p-10 bg-gray-300 overflow-hidden"
+                    style={{ boxShadow: 'rgba(149, 157, 165, 0.3) 0px 4px 24px' }}
+                    >
+                        <div className="absolute w-[500px] md:w-[800px] right-0 top-[50%] -translate-y-1/2 translate-x-1/2 lg:translate-x-1/3 opacity-30">
+                            <SlowMotion className="w-full">
+                                <img
+                                src="/images/uuundulate9-dark.svg"
+                                alt="background"
+                                className="w-full"
+                                />
+                            </SlowMotion>
+                        </div>
+                        <h2 className="text-balance leading-normal">{subtitle}</h2>
+                    </div>
+                </div>
+                <div className="w-full px-4 md:px-16 min-[375px]:px-6 lg:px-fluid-16-64">
+                    <TwoColumnMarkdown text={description} />
+                </div>
                 <div className="mx-auto bg-current opacity-30 h-px w-[75%] max-w-[490px]"></div>
                 {additionalInfo &&
-                    <div>
+                    <div className="w-full px-4 md:px-16 min-[375px]:px-6 lg:px-fluid-16-64">
                         <Markdown>
                             {additionalInfo}
                         </Markdown>                    
                     </div>
                 }
-                <div className="w-full flex justify-between items-center mt-20">
+                <div className="w-full px-4 md:px-16 min-[375px]:px-6 lg:px-fluid-16-64 flex justify-between items-center mt-20">
                     <Link href={index === 0 ? '/projects' : resolveProjectsPathname(index - 1)}>
                         <div className="w-full flex max-[360px]:gap-1 gap-2 items-center">
                             <h2 className="scale-[-1]"><IconArrowRight size={'1em'} stroke={2} /></h2>
@@ -67,4 +111,25 @@ export default function ProjectDescription ({
                 </div>
         </div>
     )
+}
+
+const get3dCarouselItems = (images: string[]) => {
+    const carouselItems : Carousel3DSlide[] = [];
+    images.map((item, index) => {
+        carouselItems.push({
+            key: `image-${index}`,
+            content:
+                <div className="w-[400px] h-[400px] flex">
+                    <Image
+                    alt={`image-${index}`}
+                    src={item}
+                    // fill
+                    width={400}
+                    height={400}
+                    className="object-contain"
+                    />
+                </div>                
+        })
+    })
+    return carouselItems;
 }
