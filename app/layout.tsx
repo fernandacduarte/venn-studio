8f709cdeae776d0671dcff50b4c7baf9b0671b9b
Pedro Franceschin <pbfranceschin@gmail.com>
2025-04-05 06:59:26 -0300

    fix: lint no unused vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,9 +53,9 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  // children,
 }: Readonly<{
-  children: React.ReactNode;
+  // children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
